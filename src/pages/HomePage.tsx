fdc770277547ee235571fd6c5443ff4dbb3c330d
Brock Bena <bbena99@gmail.com>
2024-05-20 17:43:09 -0500

    changed from onClick to an a tag wrap. This converts the mouse cursor to a pointer to indicate a click event.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -17,34 +17,36 @@ export function HomePage(props?:PageProps):ReactNode{
       </div>
     </section>
     <section id="carousel" className="grid grid-cols-2 gap-4 justify-items-center [&_div]:my-5">
-      <Card
-        className="max-w-sm drop-shadow-md"
-        imgAlt={constants.PERSONAL_IMGS[0].alt}
-        imgSrc={constants.PERSONAL_IMGS[0].src}
-        onClick={()=>{window.location.href='/AboutMe'}}
-      >
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-between">
-          <span>About me</span>
-          <FontAwesomeIcon icon={faShare}/>
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          Learn more about me, and how I learned programming.
-        </p>
-      </Card>
-      <Card
-        className="max-w-sm drop-shadow-md"
-        imgAlt={constants.PORTFOLIO_IMGS[0].alt}
-        imgSrc={constants.PORTFOLIO_IMGS[0].src}
-        onClick={()=>{window.location.href='/Portfolio'}}
-      >
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-between">
-          <span>My Portfolio</span>
-          <FontAwesomeIcon icon={faShare}/>
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          See all the projects I have worked on.
-        </p>
-      </Card>
+      <a href="/AboutMe">
+        <Card
+          className="max-w-sm drop-shadow-md"
+          imgAlt={constants.PERSONAL_IMGS[0].alt}
+          imgSrc={constants.PERSONAL_IMGS[0].src}
+        >
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-between">
+            <span>About me</span>
+            <FontAwesomeIcon icon={faShare}/>
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Learn more about me, and how I learned programming.
+          </p>
+        </Card>
+      </a>
+      <a href="Portfolio">
+        <Card
+          className="max-w-sm drop-shadow-md"
+          imgAlt={constants.PORTFOLIO_IMGS[0].alt}
+          imgSrc={constants.PORTFOLIO_IMGS[0].src}
+        >
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-between">
+            <span>My Portfolio</span>
+            <FontAwesomeIcon icon={faShare}/>
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            See all the projects I have worked on.
+          </p>
+        </Card>
+      </a>
       <Card className="grid-column: col-span-2">
         <h2 className="max-w-screen-xl w-full flex flex-wrap mx-auto justify-between p-4 self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
           About my website
