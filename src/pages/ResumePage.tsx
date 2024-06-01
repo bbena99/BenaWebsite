a45c74928f5c135753a0aa73d249ee375f839683
Brock Bena <bbena99@gmail.com>
2024-05-31 21:40:52 -0500

    Finished ResumePage

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -3,18 +3,28 @@ import Resume from '../assets/BrockResume.pdf';
 import { Worker } from '@react-pdf-viewer/core';
 import { Viewer } from "@react-pdf-viewer/core";
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 export function ResumePage():ReactNode{
 
   return (
     <>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-        <Viewer 
-          fileUrl={Resume}
-          plugins={[
-          ]}
-        />
-      </Worker>
+      <div className="mx-24 my-6">
+        <a href={Resume} className="underline text-sky-800 dark:text-sky-400">
+          Download my Resume
+          <FontAwesomeIcon icon={faArrowRightToBracket} className="rotate-90"/>
+        </a>
+      </div>
+      <div className="flex mx-24 my-6">
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+          <Viewer 
+            fileUrl={Resume}
+            plugins={[
+            ]}
+            />
+        </Worker>
+      </div>
     </>
   )
 }
