3939c5fff0ec62c5d916b151d2dd72363b0539e1
Brock Bena <bbena99@gmail.com>
2024-06-04 19:41:43 -0500

    Finalized ResumePage

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -1,32 +1,7 @@
 import { ReactNode } from "react";
 import Resume from '../assets/BrockResume.pdf';
-import { Worker } from '@react-pdf-viewer/core';
-import { Viewer } from "@react-pdf-viewer/core";
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 export function ResumePage():ReactNode{
-
-  return (
-    <>
-      <div className="mx-24 my-4">
-        <a href={Resume} className="underline text-sky-800 dark:text-sky-400">
-          Download my Resume
-          <FontAwesomeIcon icon={faArrowRightToBracket} className="rotate-90"/>
-        </a>
-      </div>
-      <div className="flex mx-24 my-6">
-        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.worker.min.js">
-          <Viewer fileUrl={Resume}/>
-        </Worker>
-      </div>
-      <div className="mx-24 my-4">
-        <a href={Resume} className="underline text-sky-800 dark:text-sky-400">
-          Download my Resume
-          <FontAwesomeIcon icon={faArrowRightToBracket} className="rotate-90"/>
-        </a>
-      </div>
-    </>
-  )
+  //You should have seen the look on my face when I saw you can use iframe as a standard pdf viewer...
+  return <iframe src={Resume} className="w-full screen"/>
 }
