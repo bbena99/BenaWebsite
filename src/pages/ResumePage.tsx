8ea9b6927cdbfe540730f6f676cfa733278406a9
Brock Bena <bbena99@gmail.com>
2024-06-12 11:05:25 -0500

    Moved .screen to be a style in resume since it was only used in 1 location.

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -3,5 +3,5 @@ import Resume from '../assets/BrockResume.pdf';
 
 export function ResumePage():ReactNode{
   //You should have seen the look on my face when I saw you can use iframe as a standard pdf viewer...
-  return <iframe src={Resume} className="w-full screen"/>
+  return <iframe src={Resume} className="w-full" style={{height:"calc(100vh - 5rem)"}}/>
 }
